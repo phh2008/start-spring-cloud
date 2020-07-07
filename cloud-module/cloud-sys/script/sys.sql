eac3f5dabd01c3f4509d2ccad300413c10f41ba0
penghuihui <phh2008@vip.qq.com>
2020-07-07 15:07:40 +0800

    add test sql file

---
+++ cloud-module/cloud-sys/script/sys.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `sys_log` (
+  `id` bigint(20) NOT NULL,
+  `log_type` tinyint(1) DEFAULT NULL COMMENT '日志类型（1登录日志，2操作日志）',
+  `log_content` varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '日志内容',
+  `operate_type` tinyint(1) DEFAULT NULL COMMENT '操作类型',
+  `user_id` bigint(20) DEFAULT NULL COMMENT '操作用户账号',
+  `username` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '操作用户名称',
+  `ip` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT 'IP',
+  `method` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '请求java方法',
+  `request_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '请求路径',
+  `request_param` longtext CHARACTER SET utf8 COLLATE utf8_general_ci COMMENT '请求参数',
+  `request_type` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '请求类型',
+  `cost_time` bigint(20) DEFAULT NULL COMMENT '耗时',
+  `create_by` bigint(20) DEFAULT NULL COMMENT '创建人',
+  `created_at` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_by` bigint(20) DEFAULT NULL COMMENT '更新人',
+  `updated_at` datetime DEFAULT NULL COMMENT '更新时间',
+  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除状态：0-正常，1-已删除',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `index_log_type` (`log_type`) USING BTREE,
+  KEY `index_operate_type` (`operate_type`) USING BTREE,
+  KEY `index_user_id` (`user_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='系统日志表';
