e43e45df24e6b595ced8b2042f7ec19f6ec520e8
penghuihui <phh2008@vip.qq.com>
2020-07-09 14:34:23 +0800

    update BaseController.java

--- cloud-core/core-boot/src/main/java/org/example/core/boot/handler/BaseController.java
+++ cloud-core/core-boot/src/main/java/org/example/core/boot/handler/BaseController.java
@@ -1,5 +1,10 @@
 package org.example.core.boot.handler;
 
+import org.example.core.common.context.UserContextHandler;
+import org.example.core.common.jwt.IJwtInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * 控制器基类
  *
@@ -8,4 +13,25 @@ package org.example.core.boot.handler;
  * @date 2020/6/28
  */
 public class BaseController {
+
+    public Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * 获取登录用户信息
+     *
+     * @return null if not login
+     */
+    public IJwtInfo getSubject() {
+        return UserContextHandler.getJwtInfo();
+    }
+
+    /**
+     * 获取登录用户信息
+     *
+     * @return CloudException if not login
+     */
+    public IJwtInfo getSubjectThrow() {
+        return UserContextHandler.getJwtInfoThrow();
+    }
+
 }
