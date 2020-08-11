d9603344045e53749b05fdf31fcc6af7b10b3fc6
penghuihui <huihui.peng@epfootwear.com>
2020-08-11 13:27:44 +0800

    update: swagger 多包扫描问题

--- cloud-core/core-boot/src/main/java/org/example/core/boot/config/SwaggerConfiguration.java
+++ cloud-core/core-boot/src/main/java/org/example/core/boot/config/SwaggerConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,6 +13,10 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * swagger 配置
@@ -25,18 +30,23 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfiguration {
 
+    private static final String SPLIT_STR = ",";
+
     @Value("${swagger.api.title}")
     private String title;
 
     @Value("${swagger.api.version}")
     private String version;
 
+    @Value("${swagger.basePackages:org.example}")
+    private String basePackages;
+
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("org.example"))
+                .apis(basePackage(basePackages)::test)
                 .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
                 .paths(PathSelectors.any())
                 .build();
@@ -47,6 +57,28 @@ public class SwaggerConfiguration {
                 .title(title)
                 .version(version)
                 .build();
+    }
+
+
+    public static Predicate<RequestHandler> basePackage(final String basePackage) {
+        return input -> declaringClass(input).map(handlerPackage(basePackage)).orElse(true);
+    }
+
+    private static Function<Class<?>, Boolean> handlerPackage(final String basePackage) {
+        return input -> {
+            assert input != null;
+            for (String strPackage : basePackage.split(SPLIT_STR)) {
+                boolean isMatch = input.getPackage().getName().startsWith(strPackage);
+                if (isMatch) {
+                    return true;
+                }
+            }
+            return false;
+        };
+    }
+
+    private static Optional<Class<?>> declaringClass(RequestHandler input) {
+        return Optional.ofNullable(input.declaringClass());
     }
 
 }
